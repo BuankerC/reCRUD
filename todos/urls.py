8434746a717c3cca182dfce3b8ad0fb8a4038ac2
Seon Yang <ergaster91@gmail.com>
2019-10-02 16:50:05 +0900

    20191002 | django2

--- todos/urls.py
+++ todos/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('new/', views.new),
     path('create/', views.create),
 
-    path('<int:id>/delete', views.delete),
+    path('<int:id>/delete/', views.delete),
+
+    path('<int:id>/edit/', views.edit),
+    path('<int:id>/update/', views.update),
 ]
